f9ec2191f0b4bcc9aa65fddf527acb2488560e90
indigo <indigonakamoto@gmail.com>
2022-03-04 16:48:02 +0800

    update database config parameters

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "strapi-test-database.cta6s6tj2mqr.us-west-1.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+      database: env("DATABASE_NAME", "strapi-test-database"),
+      user: env("DATABASE_USERNAME", "Postgres"),
+      password: env("DATABASE_PASSWORD", "AnyTableL0wel"),
     },
     useNullAsDefault: true,
   },
