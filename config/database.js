a171deffce86733b6c6dc384d9e9ef36bd6ee801
indigo <indigonakamoto@gmail.com>
2022-03-05 09:15:59 +0800

    Config for digital ocean deployment

--- config/database.js
+++ config/database.js
@@ -2,11 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "strapi-test-database.cta6s6tj2mqr.us-west-1.rds.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi-test-database"),
-      user: env("DATABASE_USERNAME", "Postgres"),
-      password: env("DATABASE_PASSWORD", "AnyTableL0wel"),
+      host: "strapi-test-database.cta6s6tj2mqr.us-west-1.rds.amazonaws.com",
+      port: 5432,
+      database: "strapi-test-database",
+      user: "Postgres",
+      password: "AnyTableL0wel",
+    },
+    pool:{
+      
     },
     useNullAsDefault: true,
   },
